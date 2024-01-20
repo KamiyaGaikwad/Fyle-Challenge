d3193c91045f277dace2403831fd93665fc662fa
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2024-01-20 07:44:33 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+let githubIdName = "johnpapa";
+const apiUrl = `https://api.github.com/users/${githubIdName}/repos?per_page=100`;
+let repositories = [];
+let currentPage = 1;
+const reposPerPage = 10;
+let avatarImage = document.getElementById("avatar-image");
+let userName = document.getElementById("user-name");
+let userBio = document.getElementById("user-bio");
+let userLocation = document.getElementById("user-location");
+let userGithub = document.getElementById("user-github");
+let userTwitter = document.getElementById("user-twitter");
+const olderButton = document.getElementById('older-button');
+const newerButton = document.getElementById('newer-button');
+
+  function showLoader() {
+    document.getElementById('loader').style.display = 'block';
+  }
+
+  function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+  }
+
+  function fetchUserDetails() {
+    fetch(`https://api.github.com/users/${githubIdName}`)
+      .then(response => response.json())
+      .then(user => {
+        // var elements = document.querySelectorAll('.toggle-up');
+
+        // elements.forEach(function(element) {
+        //   element.classList.remove('toggle-up');
+        //   element.classList.add('toggle-down');
+        // });
+        avatarImage.src = `${user.avatar_url}`;
+        userName.innerText = `${user.name}`;
+        userBio.innerText = `${user.bio}`;
+        userLocation.innerText = `${user.location}`;
+        userGithub.href = `${user.html_url}`;
+        userGithub.innerText = `${user.login}`;
+        userTwitter.href = `https://twitter.com/${user.twitter_username}`;
+        userTwitter.innerText = `${user.twitter_username}`;
+      }
+      );
+  }
+
+  function fetchRepositories() {
+    showLoader();
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+          var elements = document.querySelectorAll('.toggle-up');
+  
+          elements.forEach(function(element) {
+            element.classList.remove('toggle-up');
+            element.classList.add('toggle-down');
+          });
+        repositories = data;
+        hideLoader();
+        displayRepos(currentPage);
+        displayPagination();
+      });
+  }
+
+  function displayRepos(page) {
+    const startIndex = (page - 1) * reposPerPage;
+    const endIndex = startIndex + reposPerPage;
+    const reposToDisplay = repositories.slice(startIndex, endIndex);
+
+    let repoList = '';
+    reposToDisplay.forEach(repo => {
+      repoList += `
+        <div class="mb-3 d-flex flex-column border border-dark repo-div p-3">
+          <h3 class="repo-name" title="${repo.name}">${repo.name}</h3>
+          <p>${repo.description?repo.description:""}</p>
+          <div class="d-flex flex-wrap gap-2">
+            ${repo.topics.map(topic => `<h4><span class="badge text-bg-primary p-2 pt-1">${topic}</span></h4>`).join('')}
+          </div>
+        </div>
+      `;
+    });
+
+    document.getElementById('repo-list').innerHTML = repoList;
+  }
+
+  function displayPagination() {
+    const totalPages = Math.ceil(repositories.length / reposPerPage);
+    const pagination = document.getElementById('pagination');
+
+    let paginationHtml = `
+      <ul class="pagination justify-content-center ${window.innerWidth <= 576 ? 'pagination-sm' : ''}">
+        <li class="page-item ${currentPage === 1 ? 'disabled' : ''}">
+          <a class="page-link" href="#" onclick="changePage(${currentPage - 1})" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+    `;
+
+    for (let i = 1; i <= totalPages; i++) {
+      paginationHtml += `
+        <li class="page-item ${currentPage === i ? 'active' : ''}">
+          <a class="page-link" href="#" onclick="changePage(${i})">${i}</a>
+        </li>
+      `;
+    }
+
+    paginationHtml += `
+        <li class="page-item ${currentPage === totalPages ? 'disabled' : ''}">
+          <a class="page-link" href="#" onclick="changePage(${currentPage + 1})" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    `;
+
+    pagination.innerHTML = paginationHtml;
+    olderButton.disabled = (currentPage === 1);
+    newerButton.disabled = (currentPage === totalPages);
+  }
+
+  function changePage(page) {
+    if (page >= 1 && page <= Math.ceil(repositories.length / reposPerPage)) {
+      currentPage = page;
+      displayRepos(currentPage);
+      displayPagination();
+    }
+  }
+
+  window.addEventListener('resize', function () {
+    displayPagination();
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    fetchUserDetails();
+    fetchRepositories();
+  });
